f90f41fea0226b532622147753778742d93793cc
Kalcio <mmunozm@utem.cl>
2022-04-08 20:37:12 -0400

    nusoap install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-SOAP-API',
+        'version' => 'dev-SOAP-API',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2ca749b557dbff476041f95c2b195d40f414290f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-SOAP-API',
+            'version' => 'dev-SOAP-API',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2ca749b557dbff476041f95c2b195d40f414290f',
+            'dev_requirement' => false,
+        ),
+        'econea/nusoap' => array(
+            'pretty_version' => 'v0.9.11',
+            'version' => '0.9.11.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../econea/nusoap',
+            'aliases' => array(),
+            'reference' => '2e00c785f3a0c4bb12f3fe818fbb6b3064bd045e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
