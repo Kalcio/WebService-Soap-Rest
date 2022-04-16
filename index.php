56cdc4984ce0af50b48448303da03951a0363f7b
Kalcio <mmunozm@utem.cl>
2022-04-16 19:27:55 -0400

    Modificaciones menores

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ $server->wsdl->addComplexType(
 );
 
 $server->register(
-    'separarNombres',
-    array('name' => 'tns:nombres'),
-    array('name' => 'tns:response'),
+    'validarRut',
+    array('rut' => 'xsd:string', 'dv' => 'xsd:string'),
+    array('return' => 'xsd:string'),
     $namespace,
     false,
     'rpc',
@@ -29,15 +29,41 @@ $server->register(
     'Recibe un nombre y los separa',
 );
 
-function separarNombres($names){
-    // $separador = ' ';
-    // $separados = explode($separador, $names);
+// $server->register(
+//     'separarNombres',
+//     array('name' => 'tns:nombres'),
+//     array('nombre' => 'tns:nombre', 'apellido1' => 'tns:apellido1', 'apellido2' => 'tns:apellidos2'),
+//     $namespace,
+//     false,
+//     'rpc',
+//     'encoded',
+//     'Recibe un nombre y los separa',
+// );
 
-    // if(count($separados)<3){
 
-    // }
-    return array("Numero de Intentos");
+function validarRut($rut, $dv){
+    $r=(int)$rut;
+    $d=(int)$dv;
+    $s=1;
+     for($m=0;$r!=0;$r/=10)
+        $s=($s+$r%10*(9-$m++%6))%11;
+     if($dv == chr($s?$s+47:75)){
+         $negativo = "El rut: " . $rut . "-" . $dv . " es VALIDO";
+        return $negativo;
+     }else {
+        $positivo = "El rut: " . $rut . "-" . $dv . " es INVALIDO";
+        return $positivo;
+     }
 }
+
+// function separarNombres($names){
+//     $separador = ' ';
+//     $separados = explode($separador, $names);
+
+//     if(count($separados)<3){
+//         return array("Error con los datos");
+//     }
+// }
 
 $POST_DATA = file_get_contents("php://input");
 $server->service($POST_DATA);
