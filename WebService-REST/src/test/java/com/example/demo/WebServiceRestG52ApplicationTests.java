dfad9ca805ddee511f8c3ac5db7681c9d9da1a5c
Cristobal Andrés Ramirez González <72883591+CristRamirezG@users.noreply.github.com>
2022-04-28 16:20:33 -0400

    cambio de versión Java
    Se Reorganizo la estructura del proyecto y se cambió de la versión 11 de java a 1.8.

---
+++ WebService-REST/src/test/java/com/example/demo/WebServiceRestG52ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.models.Rut;
+import com.example.demo.services.RutService;
+
+@SpringBootTest
+class WebServiceRestG52ApplicationTests {
+
+	@Test
+	void RutServiceTest() {
+		RutService rutService = new RutService();
+		Rut rut = new Rut();
+		Rut rutAux = new Rut();
+		rut.setRut(20107983);
+		rutAux = rutService.verifyRut(rut);
+		boolean validate = rutAux.getValidate();
+		if(validate == true) {
+			System.out.println("RutService Esta validado.");
+		}
+		
+	}
+
+}
