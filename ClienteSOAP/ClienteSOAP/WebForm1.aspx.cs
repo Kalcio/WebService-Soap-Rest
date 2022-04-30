7b64314c70bd877f2f207d274768b81ba203ef6f
Kalcio <mmunozm@utem.cl>
2022-04-30 14:59:47 -0400

    Cliente SOAP

---
+++ ClienteSOAP/ClienteSOAP/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClienteSOAP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebServiceSOAP.Soap ws = new WebServiceSOAP.Soap();
+            var result = ws.validarRut(Rut.Text, DV.Text);
+            Label1.Text = result;
+        }
+    }
+}
