3c036cf4cc3efd7c3474537107569e4e886156cc
Cristobal Andrés Ramirez González <72883591+CristRamirezG@users.noreply.github.com>
2022-04-30 18:15:54 -0400

    Cambios Menores

--- Client-REST/app.js
+++ Client-REST/app.js
@@ -11,18 +11,18 @@ var errorRut = document.querySelector('#errorRut')
 
         
 function jsonRutGet(){
-    console.log('-.-<Se llamo a la función jsonRutGet()>-.-')
-    writeTextLog ('-.-<Se llamo a la función jsonRutGet()>-.-')
+    console.log('-.-< Se llamo a la función jsonRutGet() >-.-')
+    writeTextLog ('-.-< Se llamo a la función jsonRutGet() >-.-')
     fetch(url + 'Rut')
     .then(data => data.json())
     .then(data => {
-        console.log('Informacion por pantalla: ' + ' Rut:'+ data.rut + ' Digito Verificador:' + data.verifyCode + ' Validate:' +data.validate)
-        writeTextLog ('Informacion por pantalla: ' + ' Rut:'+ data.rut + ' Digito Verificador:' + data.verifyCode + ' Validate:' +data.validate)
+        console.log('Información por pantalla: ' + ' Rut:'+ data.rut + ' Digito Verificador:' + data.verifyCode + ' Validate:' +data.validate)
+        writeTextLog ('Información por pantalla: ' + ' Rut:'+ data.rut + ' Digito Verificador:' + data.verifyCode + ' Validate:' +data.validate)
         contenidoRut.innerHTML = `
         <div class="alert alert-info text-light bg-dark" role="alert">
-        <p>el rut que se encuentra ahora mismo en el WS es : ${data.rut}</p>
+        <p>el Rut que se encuentra ahora mismo en el WS es : ${data.rut}</p>
         <p>El digito Verificador es: ${data.verifyCode}</p>
-        <p>El Rut Se ecuentra validado: ${data.validate}</p>
+        <p>El Rut Se esta validado?:  ${data.validate}</p>
         </div>
         `
     })
@@ -37,13 +37,13 @@ formularioRut.addEventListener('submit',function(e){
     var rut = datos.get('InputRut')
     var verifyCode = rut.charAt(rut.length -1);
     var verifyCodeTarget = ['1','2','3','4','5','6','7','8','9','0','k','K'];
-    min=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v",
+    var min=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v",
     "w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N",
     "O","P","Q","R","S","U","V","W","X","Y","Z"];
 
     if(rut.includes('-') && contains(verifyCode,verifyCodeTarget) && !contains(rut,min) && rut.length == 9 || rut.length == 10){
-        console.log('El Formato de los Datos de Rut Es correcto, y se envio para su evaluacio...')
-        writeTextLog('El Formato de los Datos de Rut Es correcto, y se envio para su evaluacio...')
+        console.log('El Formato de los Datos de Rut Es correcto, y se envió para su evaluación...')
+        writeTextLog('El Formato de los Datos de Rut Es correcto, y se envió para su evaluación...')
         rut = rut.slice(0, -2)
 
         const jsonRutPost = new Request(url + 'Rut', {
@@ -54,16 +54,16 @@ formularioRut.addEventListener('submit',function(e){
             },
             body: JSON.stringify({rut: rut, verifyCode: verifyCode})
         });
-        console.log('Respuesta/GET WS Metodo Rut:')
-        writeTextLog('Respuesta/GET WS Metodo Rut:')
+        console.log('Respuesta/GET WS Método Rut:')
+        writeTextLog('Respuesta/GET WS Método  Rut:')
         fetch(jsonRutPost)
         .then(response => {
             if (!response.ok) throw Error(response.status);
             return response;
         })
-        .then(response => {console.log("Se Envio Correctamente el metodo POST")
-        writeTextLog("Se Envio Correctamente el metodo POST")
-        errorRut.innerHTML=`<div class="alert alert-success text-light bg-dark" role="alert"> Se Envio Correctamente el metodo POST</div>
+        .then(_response => {console.log("Se Envió Correctamente el método POST")
+        writeTextLog("Se Envió Correctamente el método POST")
+        errorRut.innerHTML=`<div class="alert alert-success text-light bg-dark" role="alert"> Se Envió Correctamente el método POST</div>
         `
         jsonRutGet();
         })
@@ -93,12 +93,12 @@ function download(strData, strFileName, strMimeType) {
     var D = document,
         A = arguments,
         a = D.createElement("a"),
-        d = A[0],
-        n = A[1],
-        t = A[2] || "text/plain";
+        
+        n = A[1]
+        
 
     //build download link:
-    a.href = "data:" + strMimeType + "charset=utf-8," + escape(strData);
+    a.href = "data:" + strMimeType + "charset=utf-8," + (strData);
 
 
     if (window.MSBlobBuilder) { // IE10
@@ -114,20 +114,18 @@ function download(strData, strFileName, strMimeType) {
         a.innerHTML = "downloading...";
         D.body.appendChild(a);
         setTimeout(function() {
-            var e = D.createEvent("MouseEvents");
-            e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            a.dispatchEvent(e);
+            a.click();
             D.body.removeChild(a);
         }, 66);
         return true;
-    }; /* end if('download' in a) */
+    } /* end if('download' in a) */
 
 
 
     //do iframe dataURL download: (older W3)
     var f = D.createElement("iframe");
     D.body.appendChild(f);
-    f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64" : "") + "," + (window.btoa ? window.btoa : escape)(strData);
+    f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64" : "") + "," ;
     setTimeout(function() {
         D.body.removeChild(f);
     }, 333);
@@ -151,7 +149,7 @@ function jsonSplitGet(){
     fetch(url + 'Split')
     .then(data => data.json())
     .then(data => {
-        var nombres = data.nombres;
+
         data.nombres.forEach(element => {
             contenidoSplit.innerHTML = `${element}`
         });
@@ -159,7 +157,7 @@ function jsonSplitGet(){
         writeTextLog('Nombres: ' + data.nombres + ' Apellido Paterno: ' + data.paterno + ' Apellido Materno: ' + data.materno)
         contenidoSplit.innerHTML = ` <div class="alert alert-info text-light bg-dark" role="alert">`
         
-        + `<p>Los Nombres Registrados son ${data.nombres}<p>` +
+        + `<p>Los Nombres Registrados son: ${data.nombres}<p>` +
         `<p>Apellido Paterno : ${data.paterno}</p>
         <p>Apellido Materno: ${data.materno}</p>
         </div>
@@ -174,12 +172,12 @@ formularioSplit.addEventListener('submit',function(e){
     e.preventDefault();                          //evita que se envie el formulario vacio
     var datos = new FormData(formularioSplit)        
     var InputSplit = datos.get('InputSplit')
-    var iChars = "@#$%^&*()+=-]\\[\';,!/}|\"{:<>?";
-    countSplit = (InputSplit.match(/ /g) || []).length;
+    
+    var countSplit = (InputSplit.match(/ /g) || []).length;
 
     if(InputSplit.includes(' ') && countSplit >= 2 ) {    
-        console.log('El Formato de los Datos de Split Es correcto, y se envio para su evaluacio...')
-        writeTextLog('El Formato de los Datos de Split Es correcto, y se envio para su evaluacio...')
+        console.log('El Formato de los Datos de Split Es correcto, y se envió para su evaluación...')
+        writeTextLog('El Formato de los Datos de Split Es correcto, y se envió para su evaluación...')
         const jsonSplitPost = new Request(url + 'Split', {
             method: 'POST',
             headers: {
@@ -188,16 +186,16 @@ formularioSplit.addEventListener('submit',function(e){
             },
             body: JSON.stringify({nombreCompleto: InputSplit})
         });
-        console.log('Respuesta/GET WS Metodo Split:')
-        writeTextLog('Respuesta/GET WS Metodo Split:')
+        console.log('Respuesta/GET WS Método Split:')
+        writeTextLog('Respuesta/GET WS Método Split:')
         fetch(jsonSplitPost)
         .then(response => {
             if (!response.ok) throw Error(response.status);
             return response;
         })
-        .then(response => {console.log("Se Envio Correctamente el metodo POST")
-        writeTextLog("Se Envio Correctamente el metodo POST")
-        errorSplit.innerHTML=`<div class="alert alert-success text-light bg-dark" role="alert"> Se Envio Correctamente el metodo POST</div>
+        .then(_response => {console.log("Se Envió Correctamente el método POST")
+        writeTextLog("Se Envió Correctamente el método POST")
+        errorSplit.innerHTML=`<div class="alert alert-success text-light bg-dark" role="alert"> Se Envió Correctamente el método POST</div>
         `
         jsonSplitGet();
         })
